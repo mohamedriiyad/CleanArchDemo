729a59212a50c3190aa42bfd23a6a919ef9e77dd
Mohamed Riyad <mohamedriiyad@gmail.com>
2021-02-07 18:53:34 +0200

    Commit Implementation of Sevices and Repository GetCourses Methods

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using CleanArch.Domain;
+using CleanArch.Domain.Interfaces;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository :ICourseRepository
+    {
+        private readonly UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
